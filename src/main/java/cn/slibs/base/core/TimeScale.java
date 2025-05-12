c2670a2469348b0448f5d0b900734b295f0f9e77
super_libraries <super_libraries@163.com>
2025-05-12 17:51:37 +0800

    `TimeScale`类生成：序号与时间粒度关系表

--- src/main/java/cn/slibs/base/core/TimeScale.java
+++ src/main/java/cn/slibs/base/core/TimeScale.java
@@ -1,11 +1,11 @@
 package cn.slibs.base.core;
 
+import com.iofairy.falcon.map.MapKit;
 import com.iofairy.top.G;
+import com.iofairy.top.S;
 
 import java.time.temporal.ChronoUnit;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 时间粒度
@@ -19,34 +19,34 @@ public enum TimeScale {
     SECOND15   ( 3,  14,   3,   3,    ChronoUnit.SECONDS,  15,   "yyyyMMddHHmmss",    "second15",    "15秒"),
     SECOND30   ( 4,  15,   4,   4,    ChronoUnit.SECONDS,  30,   "yyyyMMddHHmmss",    "second30",    "30秒"),
     MIN        ( 5,   1,   5,  10,    ChronoUnit.MINUTES,   1,     "yyyyMMddHHmm",         "min",    "分"),
-    MIN2       ( 6,  16,  23,  11,    ChronoUnit.MINUTES,   2,     "yyyyMMddHHmm",        "min2",    "2分钟"),
-    MIN3       ( 7,  17,  24,  12,    ChronoUnit.MINUTES,   3,     "yyyyMMddHHmm",        "min3",    "3分钟"),
-    MIN4       ( 8,  18,  25,  13,    ChronoUnit.MINUTES,   4,     "yyyyMMddHHmm",        "min4",    "4分钟"),
+    MIN2       ( 6,  16,  29,  11,    ChronoUnit.MINUTES,   2,     "yyyyMMddHHmm",        "min2",    "2分钟"),
+    MIN3       ( 7,  17,  30,  12,    ChronoUnit.MINUTES,   3,     "yyyyMMddHHmm",        "min3",    "3分钟"),
+    MIN4       ( 8,  18,  31,  13,    ChronoUnit.MINUTES,   4,     "yyyyMMddHHmm",        "min4",    "4分钟"),
     MIN5       ( 9,   2,   6,  14,    ChronoUnit.MINUTES,   5,     "yyyyMMddHHmm",        "min5",    "5分钟"),
     MIN10      (10,   3,   7,  15,    ChronoUnit.MINUTES,  10,     "yyyyMMddHHmm",       "min10",    "10分钟"),
     MIN15      (11,   4,   8,  16,    ChronoUnit.MINUTES,  15,     "yyyyMMddHHmm",       "min15",    "15分钟"),
     MIN30      (12,   5,   9,  17,    ChronoUnit.MINUTES,  30,     "yyyyMMddHHmm",       "min30",    "30分钟"),
     HOUR       (13,   6,  10,  20,      ChronoUnit.HOURS,   1,       "yyyyMMddHH",        "hour",    "时"),
-    HOUR2      (14,  19,  26,  21,      ChronoUnit.HOURS,   2,       "yyyyMMddHH",       "hour2",    "2小时"),
-    HOUR3      (15,  20,  27,  22,      ChronoUnit.HOURS,   3,       "yyyyMMddHH",       "hour3",    "3小时"),
-    HOUR4      (16,  21,  28,  23,      ChronoUnit.HOURS,   4,       "yyyyMMddHH",       "hour4",    "4小时"),
-    HOUR5      (17,  22,  29,  24,      ChronoUnit.HOURS,   5,       "yyyyMMddHH",       "hour5",    "5小时"),
-    HOUR6      (18,  23,  11,  25,      ChronoUnit.HOURS,   6,       "yyyyMMddHH",       "hour6",    "6小时"),
-    HOUR12     (19,  24,  12,  26,      ChronoUnit.HOURS,  12,       "yyyyMMddHH",      "hour12",    "12小时"),
-    DAY        (20,   7,  13,  30,       ChronoUnit.DAYS,   1,         "yyyyMMdd",         "day",    "天"),
-    DAY5       (21,  25,  14,  31,       ChronoUnit.DAYS,   5,         "yyyyMMdd",        "day5",    "5天"),
-    DAY10      (22,  26,  30,  32,       ChronoUnit.DAYS,  10,         "yyyyMMdd",       "day10",    "10天"),
-    DAY15      (23,  27,  31,  33,       ChronoUnit.DAYS,  15,         "yyyyMMdd",       "day15",    "15天"),
-    WEEK       (24,   8,  15,  34,      ChronoUnit.WEEKS,   1,         "yyyyMMdd",        "week",    "周"),
-    MONTH      (25,   9,  16,  40,     ChronoUnit.MONTHS,   1,           "yyyyMM",       "month",    "月"),
-    MONTH2     (26,  28,  32,  41,     ChronoUnit.MONTHS,   2,           "yyyyMM",      "month2",    "2个月"),
-    MONTH3     (27,  29,  17,  42,     ChronoUnit.MONTHS,   3,           "yyyyMM",      "month3",    "3个月"),
-    MONTH6     (28,  30,  18,  43,     ChronoUnit.MONTHS,   6,           "yyyyMM",      "month6",    "6个月"),
-    MONTH_DAY  (29,  10,  19,  44,     ChronoUnit.MONTHS,   1,         "yyyyMM01",    "monthDay",    "月01日"),
-    MONTH_DAY2 (30,  31,  33,  45,     ChronoUnit.MONTHS,   2,         "yyyyMM01",   "monthDay2",    "2个月01日"),
-    MONTH_DAY3 (31,  32,  20,  46,     ChronoUnit.MONTHS,   3,         "yyyyMM01",   "monthDay3",    "3个月01日"),
-    MONTH_DAY6 (32,  33,  21,  47,     ChronoUnit.MONTHS,   6,         "yyyyMM01",   "monthDay6",    "6个月01日"),
-    YEAR       (33,  11,  22,  50,      ChronoUnit.YEARS,   1,             "yyyy",        "year",    "年"),
+    HOUR2      (14,  19,  11,  21,      ChronoUnit.HOURS,   2,       "yyyyMMddHH",       "hour2",    "2小时"),
+    HOUR3      (15,  20,  12,  22,      ChronoUnit.HOURS,   3,       "yyyyMMddHH",       "hour3",    "3小时"),
+    HOUR4      (16,  21,  32,  23,      ChronoUnit.HOURS,   4,       "yyyyMMddHH",       "hour4",    "4小时"),
+    HOUR5      (17,  22,  33,  24,      ChronoUnit.HOURS,   5,       "yyyyMMddHH",       "hour5",    "5小时"),
+    HOUR6      (18,  23,  13,  25,      ChronoUnit.HOURS,   6,       "yyyyMMddHH",       "hour6",    "6小时"),
+    HOUR12     (19,  24,  14,  26,      ChronoUnit.HOURS,  12,       "yyyyMMddHH",      "hour12",    "12小时"),
+    DAY        (20,   7,  15,  30,       ChronoUnit.DAYS,   1,         "yyyyMMdd",         "day",    "天"),
+    DAY5       (21,  25,  16,  31,       ChronoUnit.DAYS,   5,         "yyyyMMdd",        "day5",    "5天"),
+    DAY10      (22,  26,  17,  32,       ChronoUnit.DAYS,  10,         "yyyyMMdd",       "day10",    "10天"),
+    DAY15      (23,  27,  18,  33,       ChronoUnit.DAYS,  15,         "yyyyMMdd",       "day15",    "15天"),
+    WEEK       (24,   8,  19,  34,      ChronoUnit.WEEKS,   1,         "yyyyMMdd",        "week",    "周"),
+    MONTH      (25,   9,  20,  40,     ChronoUnit.MONTHS,   1,           "yyyyMM",       "month",    "月"),
+    MONTH2     (26,  28,  21,  41,     ChronoUnit.MONTHS,   2,           "yyyyMM",      "month2",    "2个月"),
+    MONTH3     (27,  29,  22,  42,     ChronoUnit.MONTHS,   3,           "yyyyMM",      "month3",    "3个月"),
+    MONTH6     (28,  30,  23,  43,     ChronoUnit.MONTHS,   6,           "yyyyMM",      "month6",    "6个月"),
+    MONTH_DAY  (29,  10,  24,  44,     ChronoUnit.MONTHS,   1,         "yyyyMM01",    "monthDay",    "月01日"),
+    MONTH_DAY2 (30,  31,  25,  45,     ChronoUnit.MONTHS,   2,         "yyyyMM01",   "monthDay2",    "2个月01日"),
+    MONTH_DAY3 (31,  32,  26,  46,     ChronoUnit.MONTHS,   3,         "yyyyMM01",   "monthDay3",    "3个月01日"),
+    MONTH_DAY6 (32,  33,  27,  47,     ChronoUnit.MONTHS,   6,         "yyyyMM01",   "monthDay6",    "6个月01日"),
+    YEAR       (33,  11,  28,  50,      ChronoUnit.YEARS,   1,             "yyyy",        "year",    "年"),
     IRREGULAR  (-1,  -1,  -1,  -1,                  null,  -1,               null,   "irregular",    "不定时"),
     ;
 
@@ -67,6 +67,10 @@ public enum TimeScale {
     public static final Map<Integer, TimeScale> UNIT_INDEX_TS;
     public static final Map<String, TimeScale>  EN_NAME_TS;
     public static final Map<String, TimeScale>  CH_NAME_TS;
+    /**
+     * 序号与时间粒度关系表
+     */
+    public static final String TS_TABLE;
 
     static {
         Map<Integer, TimeScale> valueMap        = new LinkedHashMap<>();
@@ -96,6 +100,8 @@ public enum TimeScale {
         UNIT_INDEX_TS   = Collections.unmodifiableMap(unitIndexMap);
         EN_NAME_TS      = Collections.unmodifiableMap(enNameMap);
         CH_NAME_TS      = Collections.unmodifiableMap(chNameMap);
+
+        TS_TABLE = generateTsTable();
     }
 
     TimeScale(int value, int often1Index, int often2Index, int unitIndex, ChronoUnit chronoUnit, int timeAmount, String dateFormat, String enName, String chName) {
@@ -169,6 +175,54 @@ public enum TimeScale {
      */
     public static TimeScale chName2TS(String chName) {
         return G.isEmpty(chName) ? null : CH_NAME_TS.get(chName);
+    }
+
+    /**
+     * 获取序号与时间粒度关系表
+     *
+     * @return 序号与时间粒度关系表
+     */
+    private static String generateTsTable() {
+        try {
+            List<Map.Entry<Integer, TimeScale>> indexTsEntries = MapKit.sortBy(TimeScale.INDEX_TS, Comparator.comparingInt(Map.Entry::getKey));
+            List<Map.Entry<Integer, TimeScale>> often1IndexTsEntries = MapKit.sortBy(TimeScale.OFTEN1_INDEX_TS, Comparator.comparingInt(Map.Entry::getKey));
+            List<Map.Entry<Integer, TimeScale>> often2IndexTsEntries = MapKit.sortBy(TimeScale.OFTEN2_INDEX_TS, Comparator.comparingInt(Map.Entry::getKey));
+            List<Map.Entry<Integer, TimeScale>> unitIndexTsEntries = MapKit.sortBy(TimeScale.UNIT_INDEX_TS, Comparator.comparingInt(Map.Entry::getKey));
+
+            final String tableLine = "-------------------------------------------------------------------------";
+            final String tableTitle = "|        TS       |    OFTEN1_TS    |    OFTEN2_TS    |     UNIT_TS     |";
+
+            final StringBuilder tsTable = new StringBuilder();
+            tsTable.append(tableLine).append("\n");
+            tsTable.append(tableTitle).append("\n");
+            tsTable.append(tableLine).append("\n");
+
+            for (int i = 0; i < TimeScale.values().length; i++) {
+                indexTsToColumn(tsTable, indexTsEntries.get(i));
+                indexTsToColumn(tsTable, often1IndexTsEntries.get(i));
+                indexTsToColumn(tsTable, often2IndexTsEntries.get(i));
+                indexTsToColumn(tsTable, unitIndexTsEntries.get(i));
+                tsTable.append("|").append("\n");
+            }
+
+            tsTable.append(tableLine);
+
+            return tsTable.toString();
+        } catch (Throwable e) {
+            return "";
+        }
+    }
+
+    private static void indexTsToColumn(final StringBuilder tsTable, final Map.Entry<Integer, TimeScale> indexTsEntry) {
+        final int indexLength = 2;
+        final int tsNameLength = 12;
+        final char padChar = ' ';
+        final String separatorForIndexAndTs = ": ";
+
+        tsTable.append("| ")
+                .append(S.padLeftChars(indexTsEntry.getKey(), padChar, indexLength))
+                .append(separatorForIndexAndTs)
+                .append(S.padRightChars(indexTsEntry.getValue().toString(), padChar, tsNameLength));
     }
 
 
--- src/test/java/cn/slibs/test/TimeScaleTest.java
+++ src/test/java/cn/slibs/test/TimeScaleTest.java
@@ -3,10 +3,7 @@ package cn.slibs.test;
 import cn.slibs.base.core.TimeScale;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,12 +18,12 @@ public class TimeScaleTest {
 
     @Test
     void testTimeScale() {
-        System.out.println(TimeScale.INDEX_TS);
-        System.out.println(TimeScale.OFTEN1_INDEX_TS);
-        System.out.println(TimeScale.OFTEN2_INDEX_TS);
-        System.out.println(TimeScale.UNIT_INDEX_TS);
-        System.out.println(TimeScale.EN_NAME_TS);
-        System.out.println(TimeScale.CH_NAME_TS);
+        System.out.println("INDEX_TS       : " + TimeScale.INDEX_TS);
+        System.out.println("OFTEN1_INDEX_TS: " + TimeScale.OFTEN1_INDEX_TS);
+        System.out.println("OFTEN2_INDEX_TS: " + TimeScale.OFTEN2_INDEX_TS);
+        System.out.println("UNIT_INDEX_TS  : " + TimeScale.UNIT_INDEX_TS);
+        System.out.println("EN_NAME_TS     : " + TimeScale.EN_NAME_TS);
+        System.out.println("CH_NAME_TS     : " + TimeScale.CH_NAME_TS);
         System.out.println(TimeScale.CH_NAME_TS.keySet());
 
         assertEquals(TimeScale.INDEX_TS.size(), 35);
@@ -74,6 +71,51 @@ public class TimeScaleTest {
             assertSame(UnsupportedOperationException.class, e.getClass());
         }
 
+    }
+
+    @Test
+    void testTimeScaleTable() {
+        System.out.println("● 序号与时间粒度关系表\n" + TimeScale.TS_TABLE);
+        assertEquals(TimeScale.TS_TABLE,
+                "-------------------------------------------------------------------------\n" +
+                "|        TS       |    OFTEN1_TS    |    OFTEN2_TS    |     UNIT_TS     |\n" +
+                "-------------------------------------------------------------------------\n" +
+                "| -1: IRREGULAR   | -1: IRREGULAR   | -1: IRREGULAR   | -1: IRREGULAR   |\n" +
+                "|  0: SECOND      |  0: SECOND      |  0: SECOND      |  0: SECOND      |\n" +
+                "|  1: SECOND5     |  1: MIN         |  1: SECOND5     |  1: SECOND5     |\n" +
+                "|  2: SECOND10    |  2: MIN5        |  2: SECOND10    |  2: SECOND10    |\n" +
+                "|  3: SECOND15    |  3: MIN10       |  3: SECOND15    |  3: SECOND15    |\n" +
+                "|  4: SECOND30    |  4: MIN15       |  4: SECOND30    |  4: SECOND30    |\n" +
+                "|  5: MIN         |  5: MIN30       |  5: MIN         | 10: MIN         |\n" +
+                "|  6: MIN2        |  6: HOUR        |  6: MIN5        | 11: MIN2        |\n" +
+                "|  7: MIN3        |  7: DAY         |  7: MIN10       | 12: MIN3        |\n" +
+                "|  8: MIN4        |  8: WEEK        |  8: MIN15       | 13: MIN4        |\n" +
+                "|  9: MIN5        |  9: MONTH       |  9: MIN30       | 14: MIN5        |\n" +
+                "| 10: MIN10       | 10: MONTH_DAY   | 10: HOUR        | 15: MIN10       |\n" +
+                "| 11: MIN15       | 11: YEAR        | 11: HOUR2       | 16: MIN15       |\n" +
+                "| 12: MIN30       | 12: SECOND5     | 12: HOUR3       | 17: MIN30       |\n" +
+                "| 13: HOUR        | 13: SECOND10    | 13: HOUR6       | 20: HOUR        |\n" +
+                "| 14: HOUR2       | 14: SECOND15    | 14: HOUR12      | 21: HOUR2       |\n" +
+                "| 15: HOUR3       | 15: SECOND30    | 15: DAY         | 22: HOUR3       |\n" +
+                "| 16: HOUR4       | 16: MIN2        | 16: DAY5        | 23: HOUR4       |\n" +
+                "| 17: HOUR5       | 17: MIN3        | 17: DAY10       | 24: HOUR5       |\n" +
+                "| 18: HOUR6       | 18: MIN4        | 18: DAY15       | 25: HOUR6       |\n" +
+                "| 19: HOUR12      | 19: HOUR2       | 19: WEEK        | 26: HOUR12      |\n" +
+                "| 20: DAY         | 20: HOUR3       | 20: MONTH       | 30: DAY         |\n" +
+                "| 21: DAY5        | 21: HOUR4       | 21: MONTH2      | 31: DAY5        |\n" +
+                "| 22: DAY10       | 22: HOUR5       | 22: MONTH3      | 32: DAY10       |\n" +
+                "| 23: DAY15       | 23: HOUR6       | 23: MONTH6      | 33: DAY15       |\n" +
+                "| 24: WEEK        | 24: HOUR12      | 24: MONTH_DAY   | 34: WEEK        |\n" +
+                "| 25: MONTH       | 25: DAY5        | 25: MONTH_DAY2  | 40: MONTH       |\n" +
+                "| 26: MONTH2      | 26: DAY10       | 26: MONTH_DAY3  | 41: MONTH2      |\n" +
+                "| 27: MONTH3      | 27: DAY15       | 27: MONTH_DAY6  | 42: MONTH3      |\n" +
+                "| 28: MONTH6      | 28: MONTH2      | 28: YEAR        | 43: MONTH6      |\n" +
+                "| 29: MONTH_DAY   | 29: MONTH3      | 29: MIN2        | 44: MONTH_DAY   |\n" +
+                "| 30: MONTH_DAY2  | 30: MONTH6      | 30: MIN3        | 45: MONTH_DAY2  |\n" +
+                "| 31: MONTH_DAY3  | 31: MONTH_DAY2  | 31: MIN4        | 46: MONTH_DAY3  |\n" +
+                "| 32: MONTH_DAY6  | 32: MONTH_DAY3  | 32: HOUR4       | 47: MONTH_DAY6  |\n" +
+                "| 33: YEAR        | 33: MONTH_DAY6  | 33: HOUR5       | 50: YEAR        |\n" +
+                "-------------------------------------------------------------------------");
     }
 
 }
